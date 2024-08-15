dd646b2aaf215f9debec09da2fc61cecce09afc3
Picodes10 <biswalsushree000@gmail.com>
2024-08-16 03:06:27 +0530

    Add Marker & Location

--- App.js
+++ App.js
@@ -9,14 +9,22 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static('public')); // serve files from public directory
+app.use(express.static('js'));
 
 io.on("connection", function (socket) {
-  console.log("connected");
+    socket.on("send-location", function (data) {
+        io.emit("receive-location", {id: socket.id, ...data}); // send data to all connected clients
+    });
+  socket.on("disconnect", function(){
+    io.emit("user-disconnected", socket.id)
+  })
 });
 
 app.get("/", function (req, res) {
   res.render("index");
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');
+  });
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,53 @@
 const socket = io();
 
-console.log("hey");
+console.log('Socket connected:', socket.connected);
+
+// watch position
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition((position) => {
+    console.log('Geolocation update:', position.coords);
+    const { latitude, longitude } = position.coords;
+    socket.emit("send-location", { latitude, longitude });
+  }, 
+  (error) => {
+    console.error('Geolocation error:', error);
+  },
+  {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0, 
+  }
+  );
+} else {
+  console.log('Geolocation not supported');
+}
+
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Street101"
+}).addTo(map);
+
+console.log('Map initialized:', map);
+
+let markers = {};
+
+socket.on("receive-location", (data) => {
+  console.log('Received location:', data);
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude], 16);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
+
+socket.on("user-disconnected", (data) => {
+  console.log('User disconnected:', data);
+  const { id } = data;
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
